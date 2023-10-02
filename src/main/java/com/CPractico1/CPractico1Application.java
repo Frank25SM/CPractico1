d6f6ce2bfe649c15c87e9724dd80039ceb78b930
fsanchezm <fsanchezm@AUD000WTE326.bncrcp.inst.bncr.fi.cr>
2023-10-02 12:19:42 -0600

    Se creó el caso practico 1

---
+++ src/main/java/com/CPractico1/CPractico1Application.java
@@ -0,0 +1,13 @@
+package com.CPractico1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CPractico1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CPractico1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/CPractico1/CPractico1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CPractico1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CPractico1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
